7e57710b8fb4d5cba685e0584e9d0342fb7b1731
ang <ag888927@gmail.com>
2022-03-16 20:21:34 -0600

    ulti

--- app/src/main/java/com/example/pm1122restapi/ActivityImagen.java
+++ app/src/main/java/com/example/pm1122restapi/ActivityImagen.java
@@ -8,9 +8,22 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivityImagen extends AppCompatActivity {
     ImageView imageView;
@@ -30,7 +43,9 @@ public class ActivityImagen extends AppCompatActivity {
         btnGaleria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 GaleriaImagenes();
+
             }
 
 
@@ -39,11 +54,57 @@ public class ActivityImagen extends AppCompatActivity {
         btnUploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                SendImage();
             }
         });
 
     }
+
+    private void SendImage() {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, ResApiMetodos.EndPointImageUpload, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Respuesta" , response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Respuesta" , error.toString());
+            }
+        })
+        {
+            protected Map<String, String> getParams(){
+
+                String image = GetStringImage(photo);
+                Map<String, String> parametros = new HashMap<String, String>();
+
+                parametros.put("IMAGEN", image);
+
+                return parametros;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
+    }
+
+    private String GetStringImage(Bitmap photo) {
+
+        try {
+            ByteArrayOutputStream ba = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.JPEG, 70,ba);
+            byte[] imagebyte = ba.toByteArray();
+            String encode = Base64.encodeToString(imagebyte, Base64.DEFAULT);
+            return encode;
+        }catch (Exception e){
+            e.toString();
+        }
+
+
+        return  "";
+    }
+
     private void GaleriaImagenes() {
         Intent intentGaleria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intentGaleria, RESULT_IMAGE);
@@ -59,8 +120,15 @@ public class ActivityImagen extends AppCompatActivity {
         Uri imageUri;
 
         if(resultCode == RESULT_OK && requestCode == RESULT_IMAGE){
-            imageUri = data.getData();
-            imageView.setImageURI(imageUri);
+
+            try {
+                imageUri = data.getData();
+                imageView.setImageURI(imageUri);
+                photo = MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);
+            }catch (Exception ex){
+
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/pm1122restapi/ResApiMetodos.java
+++ app/src/main/java/com/example/pm1122restapi/ResApiMetodos.java
@@ -7,10 +7,14 @@ public class ResApiMetodos {
     private static final String GetEmple = "/CRUD-PHP/listaempleados.php";
     private static final String CrarEmple = "/CRUD-PHP/crear.php";
     //Metodos CRUD Rest Api para cunsumo
+    private static final String ImageUpload = "/CRUD-PHP/UploadFile.php";
 
     //Metodo Get
     public static final String EndPointGetEmple = StringHttp + ipaddress + GetEmple;
 
     //Metodo post
     public static final String EndPointCrearEmple = StringHttp + ipaddress + CrarEmple;
+
+    //Metodo para IMG
+    public static final String EndPointImageUpload = StringHttp + ipaddress + ImageUpload;
 }
